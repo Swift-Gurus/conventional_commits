646270833774f6c40b2466681cafaf94a1478fc2
Alex Crowe <alexei.hmelevski@gmail.com>
2024-03-25 16:57:58 -0400

    Fix/0/optional scope in message template (#10)
    * remove () if the scope is optional
    
    * Bump conventional_commits to 0.2.3

--- lib/commit/commit_message_generator.rb
+++ lib/commit/commit_message_generator.rb
@@ -8,7 +8,7 @@ module ConventionalCommits
       main_config = Configuration::MainConfigurationReader.new.get_configuration(path: cfg_path)
       configuration = main_config.branch
       components = BranchNameGenerator.new.branch_name_components(branch_name, path: cfg_path)
-      scope = (components[:scope] || "").downcase
+      scope = components[:scope] ? "(#{components[:scope].downcase})" : ""
       type = (components[:type] || "").downcase
       ticket_number = components[:ticket_number] || ""
       description = components[:description] || ""
@@ -19,7 +19,7 @@ module ConventionalCommits
               "The type #{type} is not allowed. Allowed types #{main_config.type.all_types}"
       end
 
-      type = "#{main_config.type.main_type(type)}(#{scope}): #{description}".strip
+      type = "#{main_config.type.main_type(type)}#{scope}: #{description}".strip
 
       ticket_ref = "#{configuration.ticket_prefix}#{configuration.ticket}#{configuration.ticket_separator}#{ticket_number}"
       body = custom_body.strip.empty? ? Configuration::DEFAULT_COMMIT_BODY_TEMPLATE : custom_body.strip
