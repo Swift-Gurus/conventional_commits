6103b4f753d6a6a35ec19271381fb64ccc3efe90
Alex Crowe <alexei.hmelevski@gmail.com>
2024-10-25 11:19:21 -0400

    access

--- lib/conventional_commits.rb
+++ lib/conventional_commits.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "conventional_commits/version"
-require_relative "configuration"
+require_relative "branch/branch_name_generator"
 module ConventionalCommits
   class Error < StandardError; end
   def self.get_branch_components(branch_name)
