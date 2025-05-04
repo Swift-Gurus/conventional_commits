a8d5b0b8146ef302e550616bf8f1f9a0fa73ae26
Alex Crowe <alexei.hmelevski@gmail.com>
2025-05-03 22:01:32 -0400

    check replace

--- lib/commit/commit_message_validator.rb
+++ lib/commit/commit_message_validator.rb
@@ -4,6 +4,7 @@ module ConventionalCommits
   class CommitMessageValidator
     def validate_commit_msg_from_file(commit_msg_path: Configuration::DEFAULT_COMMIT_MSG_PATH,
                                       cfg_path: Configuration::DEFAULT_CONFIGURATION_PATH)
+      return if commit_msg_path != Configuration::DEFAULT_COMMIT_MSG_PATH
       parser = CommitMessageParser.new
       components = parser.message_components(commit_msg_path:, cfg_path:)
 
